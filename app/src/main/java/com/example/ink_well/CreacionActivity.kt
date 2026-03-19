8584e89e8f41a04f5aefbdb23ecb52be9461d953
Alex <alejandro64.bp@gmail.com>
2026-03-19 09:55:34 -0700

    vr 1.0.4: Incorporacion de activitys Creacion y login

---
+++ app/src/main/java/com/example/ink_well/CreacionActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ink_well
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CreacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_creacion)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
